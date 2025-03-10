eaf04886da2b5f4693f5578d168461f3b402d104
mttvncnt <florestoto99@gmail.com>
2025-03-10 21:04:28 +0800

    Submitting Python Session 03 - Exercise 01

--- problem_01.py
+++ problem_01.py
@@ -1,10 +1,18 @@
-# YOUR NAME
+#Simple Calculator Program
+# FLores Matt
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
-    pass # replace this line with all of your code
+    pass  # replace this line with all of your code
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
+print("Simple Calculator Program")
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+print(f"The sum is {num1 + num2}")
+print(f"The difference is {num1 - num2}")
+print(f"The product is {num1 * num2}")
+print(f"The quotient is {(num1 / num2):.2f}")
--- problem_02.py
+++ problem_02.py
@@ -1,10 +1,17 @@
-# YOUR NAME
+#Square the Number Program
+#Enter a number: 5
+#The square of 5 is 25.00
+
+# Flores Matt
 # ITELEC2
-# Problem Set 01 - Problem 02
-# Square the Number Program
+# Problem Set 01 - Problem 01
+# Simple Calculator Program
 
 def main():
-    pass # replace this line with all of your code
+    pass  # replace this line with all of your code
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
+print("Square the Number Program")
+number = int(input("Enter a number: "))
+print(f"The square of {number} is {(number * number):.2f}")
